34da26b963011de466f5c31c233b3aaff588d502
Shukurullo2012 <shukur.ubaydullaev909@gmail.com>
2025-02-10 21:33:26 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let name = prompt("Введите ваше имя:");
+let letter = prompt("Введите букву для поиска:");
+
+if (includes(letter)) {
+    alert(`${letter}есть в имени` );
+} else {
+    alert(`${letter}есть в имени` );
+}
